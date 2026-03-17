bcc0dc364f189587f0bd646c0f2193e1102e7484
Oleg Zhuk <zhukoo@gmail.com>
2026-03-17 21:46:39 +0200

    VCST-4328: Update to .NET10

--- src/VirtoCommerce.MarketingExperienceApi.Data/Schemas/DynamicContentItemType.cs
+++ src/VirtoCommerce.MarketingExperienceApi.Data/Schemas/DynamicContentItemType.cs
@@ -1,7 +1,6 @@
 using GraphQL.Types;
 using VirtoCommerce.MarketingModule.Core.Model;
 using VirtoCommerce.Xapi.Core.Extensions;
-using VirtoCommerce.Xapi.Core.Helpers;
 using VirtoCommerce.Xapi.Core.Schemas;
 using VirtoCommerce.Xapi.Core.Services;
 
@@ -20,7 +19,7 @@ namespace VirtoCommerce.MarketingExperienceApi.Data.Schemas
             ExtendableFieldAsync<ListGraphType<DynamicPropertyValueType>>(
             "dynamicProperties",
             "Dynamic content dynamic property values",
-                QueryArgumentPresets.GetArgumentForDynamicProperties(),
+                null,
                 async context => await dynamicPropertyResolverService.LoadDynamicPropertyValues(context.Source, context.GetArgumentOrValue<string>("cultureName")));
         }
     }
